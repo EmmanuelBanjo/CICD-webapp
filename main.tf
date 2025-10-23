e4b9fbaa1f5623e0cd2829e8861c53c7672daa2e
EmmanuelBanjo <banjoemmanuel27@gmail.com>
2025-10-23 07:46:22 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "azurerm" {
 }
 
 # Resource Group
-resource "azurerm_resource_group" "rg" {
+resource "azurerm_resource_group" "rg22" {
   name     = var.resource_group_name
   location = var.location
 }
